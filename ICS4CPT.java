8b31be18d848e3d38b77de0f53ebd2497aae95b3
EWongSTA <159478843+EWongSTA@users.noreply.github.com>
2026-05-15 15:20:31 -0400

    Add files via upload

---
+++ ICS4CPT.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+
+public class ICS4CPT implements ActionListener{
+	
+	// Properties
+	JFrame theFrame = new JFrame("CPT");
+	JPanel thePanel = new JPanel();
+	Timer theTimer = new Timer(1000/60, this);
+	
+	
+	// Methods
+	public void actionPerformed(ActionEvent evt){
+		
+	}
+	
+	// COnstructor
+	public ICS4CPT() {
+		thePanel.setPreferredSize(new Dimension(1280, 720));
+		theFrame.setContentPane(thePanel);
+		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theFrame.pack();
+		theFrame.setVisible(true);
+		theTimer.start();
+	}
+	
+	
+	// Main Method
+	public static void main(String [] args){
+		new ICS4CPT();
+	}
+	
+	
+}
