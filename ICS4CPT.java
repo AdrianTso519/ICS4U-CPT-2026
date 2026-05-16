58c14412752b77c892eb3d9d3e553fbcfd18abed
EWongSTA <eeethan23@gmail.com>
2026-05-15 21:18:03 -0400

    SSM and Network started
    Added:
    - SuperSocketMaster
    - action performed methods for Jbutton, Jtextfield, and socket
    - Socket is WIP

--- ICS4CPT.java
+++ ICS4CPT.java
@@ -20,12 +20,40 @@ public class ICS4CPT implements ActionListener, MouseMotionListener, MouseListen
 	Timer theTimer = new Timer(1000/60, this);
 	
 	// Game Properties
+	JTextArea theArea = new JTextArea();
+	JScrollPane theScroll = new JScrollPane(theArea);
+	JTextField theField = new JTextField();
+	JButton theButton = new JButton("Connect");
 	
+	// Network Connection Properties
+	SuperSocketMaster Socket = null;
 	
 	
 	// Methods
 	public void actionPerformed(ActionEvent evt){
-		
+		// Field triggered
+		if(evt.getSource() == theField){
+			System.out.println("Field event triggered");
+			Socket.sendText(theField.getText());
+			theField.setText("");
+		// Button Triggered
+		}else if(evt.getSource() == theButton){
+			System.out.println("button event triggered");
+			// Establish socket network connection
+			// Construct ssm based on if they will be server or client
+			// Server
+			//Socket = new SuperSocketMaster(6767, this);
+			// client
+			Socket = new SuperSocketMaster("10.8.49.90", 6112, this);
+			// access the connect method
+			Socket.connect();
+			theButton.setEnabled(false);
+		// Socket triggered
+		}else if(evt.getSource() == Socket){
+			System.out.println("Socket event triggered");
+			String strLine = Socket.readText();
+			theArea.append(strLine + "\n");
+		}
 	}
 	
 	public void mouseMoved(MouseEvent evt){
@@ -60,13 +88,24 @@ public class ICS4CPT implements ActionListener, MouseMotionListener, MouseListen
 	
 	// Constructor
 	public ICS4CPT() {
-		thePanel.setPreferredSize(new Dimension(1280, 720));
-		theFrame.setContentPane(thePanel);
+		thePanel.setLayout(null);
+		thePanel.setPreferredSize(new Dimension(1280,720));
+		
+		theScroll.setBounds(980,0,300,300);
+		thePanel.add(theScroll);
+		
+		theField.setBounds(980,300,300,100);
+		theField.addActionListener(this);
+		thePanel.add(theField);
+		
+		theButton.setBounds(980,400,300,100);
+		theButton.addActionListener(this);
+		thePanel.add(theButton);
+		
 		theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theFrame.setContentPane(thePanel);
 		theFrame.pack();
-		theFrame.setResizable(false);
 		theFrame.setVisible(true);
-		theTimer.start();
 	}
 	
 	
